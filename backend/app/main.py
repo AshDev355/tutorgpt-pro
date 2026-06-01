6c7ea265089ce56517d05bcecf54d7e5fdc7097d
AshDev355 <ayeshasubhan354@gmail.com>
2026-06-01 14:51:27 +0500

    Connect frontend and backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+@app.get("/")
 def home():
     return {"message": "TutorGPT Backend Running"}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("");
+
+  const getMessage = async () => {
+    const response = await fetch("http://127.0.0.1:8000/");
+    const data = await response.json();
+
+    setMessage(data.message);
+  };
+
   return (
     <div>
       <h1>TutorGPT</h1>
-      <p>AI Powered Learning Assistant</p>
+
+      <button onClick={getMessage}>
+        Connect Backend
+      </button>
+
+      <h2>{message}</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
