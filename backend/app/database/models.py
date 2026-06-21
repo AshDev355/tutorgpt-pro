6253b3cbcf5bb5f43ec1e3b6700de52484319891
AshDev355 <ayeshasubhan354@gmail.com>
2026-06-21 23:16:17 +0500

    Working of upload pdf functionality

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -3,6 +3,10 @@ from sqlalchemy import Integer
 from sqlalchemy import String
 
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import ForeignKey
+
+from sqlalchemy import DateTime
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -14,3 +18,23 @@ class User(Base):
     name = Column(String)
     email = Column(String)
     password = Column(String)
+
+
+class PDF(Base):
+    __tablename__ = "pdfs"
+
+    id = Column(Integer, primary_key = True, index = True)
+
+    filename = Column(String)
+    filepath = Column(String)
+
+    size = Column(String)
+    pages = Column(Integer)
+
+    subject = Column(String, default = "General")
+    status = Column(String, default= "ready")
+
+    uploaded_at = Column(DateTime, default = datetime.utcnow)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Header, HTTPException, status, UploadFile, File
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from app.database.db import engine
-from app.database.models import Base
-from fastapi import Depends
-from sqlalchemy.orm import Session
-
-from app.database.db import SessionalLocal
-from app.database.models import User
-
-from app.schemas import UserCreate
-
-# Passlib removed completely to bypass the Windows import bug
-import bcrypt
-from app.auth.jwt import create_access_token 
-
-
-from fastapi import Header, HTTPException
-from app.auth.jwt import verify_token
-
-from fastapi import HTTPException, status
 from fastapi.security import APIKeyHeader
+from sqlalchemy.orm import Session
+import bcrypt
 import jose.jwt as jwt
+import os
+import shutil
+
+from app.database.db import engine, SessionalLocal
+from app.database.models import Base, User, PDF
+from app.schemas import UserCreate
+from app.auth.jwt import create_access_token, verify_token
+
+from typing import List
+from datetime import datetime
+
 
 # This tells FastAPI to look for an "authorization" header in Swagger
 header_scheme = APIKeyHeader(name="authorization", auto_error=False)
@@ -47,11 +41,17 @@ def get_current_user(token: str = Depends(header_scheme)):
     
     try:
         # If the token contains "Bearer ", strip it off cleanly
+        print("RAW TOKEN:", token)
+
         if token.lower().startswith("bearer "):
             token = token.split(" ")[1]
-            
+        print("TOKEN AFTER STRIP:", token)
+
         # Decode the real cryptographic payload data
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        
+        print("PAYLOAD:", payload)
+
         return payload
         
     except Exception:
@@ -60,7 +60,7 @@ def get_current_user(token: str = Depends(header_scheme)):
             detail="Could not validate credentials / Invalid Token"
         )
 
-Base.metadata.create_all(bind= engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -73,13 +73,29 @@ app.add_middleware(
 )
 
 class UserCreate(BaseModel):
-    name:str
-    email:str
-    password:str
+    name: str
+    email: str
+    password: str
 
 class UserLogin(BaseModel):
-    email:str
-    password:str
+    email: str
+    password: str
+
+
+class PDFResponse(BaseModel):
+    id: int
+    filename: str
+    filepath: str
+    size: str
+    pages: int
+    subject: str
+    status: str
+    owner_id: int
+    uploaded_at: datetime
+
+
+    class Config:
+        from_attributes = True
 
 def get_db():
     db = SessionalLocal()
@@ -89,13 +105,18 @@ def get_db():
         db.close()
 
 
+UPLOAD_DIR = "../uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+# --- ENDPOINTS ---
+
 @app.get("/")
 def home():
     return {"message": "TutorGPT Backend Running"}
 
+
 @app.post("/signup")
-def signup(user:UserCreate, db: Session =Depends(get_db)):
-    # Swapped pwd_context.hash for our clean bcrypt helper
+def signup(user: UserCreate, db: Session = Depends(get_db)):
     hashed_password = hash_password(user.password)
     new_user = User(
         name=user.name,
@@ -108,23 +129,23 @@ def signup(user:UserCreate, db: Session =Depends(get_db)):
     db.refresh(new_user)
 
     return {
-        "message":f"Welcome {new_user.name} created successfully",
-        "user_id":new_user.id
+        "message": f"Welcome {new_user.name} created successfully",
+        "user_id": new_user.id
     }
 
+
 @app.post("/login")
-def login(user:UserLogin, db: Session = Depends(get_db)):
+def login(user: UserLogin, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user.email).first()
 
     if not db_user:
         return {"error": "Invalid credentials"}
     
-    # Swapped pwd_context.verify for our clean bcrypt helper
     if not verify_password(user.password, db_user.password):
         return {"error": "Invalid credentials"}
     
     token = create_access_token(
-        data = {"user_id": db_user.id, "email": db_user.email}
+        data={"user_id": db_user.id, "email": db_user.email}
     )
 
     return {
@@ -132,9 +153,151 @@ def login(user:UserLogin, db: Session = Depends(get_db)):
         "token_type": "bearer"
     }
 
+
+@app.post("/upload")
+async def upload_pdf(
+    file: UploadFile = File(...),
+    user = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    if not file.filename.endswith(".pdf"):
+        raise HTTPException(
+            status_code=400,
+            detail="Only PDF files are allowed"
+        )
+
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    size_mb = round(file.size / (1024*1024), 1)
+    new_pdf = PDF(
+        filename=file.filename,
+        filepath=file_path,
+        size= f"{size_mb} MB",
+        pages = 0,   #temporary
+        subject = "General", #temporary
+        status = "ready",
+        owner_id=user["user_id"]
+    )
+
+    db.add(new_pdf)
+    db.commit()
+    db.refresh(new_pdf)
+
+    return {
+        "message": "PDF uploaded successfully",
+        "pdf_id": new_pdf.id,
+        "filename": new_pdf.filename
+    }
+
+
+@app.get("/my-pdfs", response_model = List[PDFResponse])
+async def get_my_pdfs(
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+##    try:
+        pdfs = db.query(PDF).filter(PDF.owner_id == current_user["user_id"]).all()
+        return pdfs
+##    except Exception as e:
+##       raise HTTPException(
+##            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR,
+##            detail= str(e)
+##        )
+
+
 @app.get("/protected")
 def protected_route(user=Depends(get_current_user)):
     return {
         "message": "You are authenticated!",
         "user": user
+    }
+
+@app.delete("/pdf/{pdf_id}")
+def delete_pdf(
+    pdf_id: int,
+    current_user = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    pdf = ( 
+        db.query(PDF).filter(
+            PDF.id == pdf_id,
+            PDF.owner_id == current_user["user_id"]  
+        ).first()
+    )
+
+    if not pdf:
+        raise HTTPException(
+            status_code = 404,
+            detail = "PDF not found"
+        )
+    if os.path.exists(pdf.filepath):
+        os.remove(pdf.filepath)
+    
+    db.delete(pdf)
+    db.commit()
+
+    return {"message": "PDF deleted"}
+
+@app.get("/dashboard")
+def dashboard(
+    current_user=Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    pdfs = (
+        db.query(PDF)
+        .filter(PDF.owner_id == current_user["user_id"])
+        .all()
+    )
+
+    return {
+        "total_pdfs": len(pdfs),
+        "subjects": len(
+            set(pdf.subject for pdf in pdfs)
+        ),
+        "ready_docs": len(
+            [p for p in pdfs if p.status == "ready"]
+        )
+    }
+
+@app.get("/dashboard-stats")
+async def dashboard_stats(
+    current_user = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    pdfs = db.query(PDF).filter(
+        PDF.owner_id == current_user["user_id"]
+    ).all()
+
+    total_pdfs = len(pdfs)
+    total_pages = sum(
+        pdf.pages if pdf.pages else 0
+        for pdf in pdfs
+    )
+
+    subjects = len(
+        set(
+            pdf.subject
+            for pdf in pdfs
+            if pdf.subject   
+        )
+    )
+
+    latest_pdf = None
+
+    if pdfs:
+        latest_pdf = max(
+            pdfs, 
+            key= lambda x: x.uploaded_at
+        ).filename
+
+
+    return {
+        "total_pdfs": total_pdfs,
+        "total_pages": total_pages,
+        "subjects": subjects,
+        "latest_pdf": latest_pdf
+
     }
